fdb5e105d0d457d4b7ccca7fb8ed65702ee7400c
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-19 19:26:35 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='xiaomi-ac2100'' package/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='openwrt'' package/default-settings/files/zzz-default-settings
 
 # 内核显示增加自己个性名称和编译日期（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
 sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
