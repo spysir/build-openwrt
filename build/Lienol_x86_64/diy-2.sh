9d3559cf959a24d543f6ad1de6d541bbb0b81ab8
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-29 20:32:14 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -2,13 +2,13 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
+# 修改openwrt登陆地址,把下面的192.168.8.5修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
+# 修改主机名字，把OpenWrt修改你喜欢的就行（不能使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/default-settings/files/zzz-default-settings
 
-# 内核显示增加自己个性名称和编译日期（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
+# 固件版本里面添加自己个性名称和编译日期（XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
 sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 
 # 修改 opentopd 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
