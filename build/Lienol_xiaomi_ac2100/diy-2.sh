41d4c4446af9be2c422d941b22e5ee76bf367ac1
weweyes <59646715+weweyes@users.noreply.github.com>
2021-01-01 13:24:29 +0800

    Update diy-2.sh

--- build/Lienol_xiaomi_ac2100/diy-2.sh
+++ build/Lienol_xiaomi_ac2100/diy-2.sh
@@ -5,8 +5,8 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='xiaomi-ac2100'' package/default-settings/files/zzz-default-settings
+# 修改主机名字，把OpenWrt修改你喜欢的就行（不能使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/default-settings/files/zzz-default-settings
 
 # 内核显示增加自己个性名称（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
 sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
