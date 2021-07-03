7ca902c01887006e772e887233e9ee6ad06d6208
weweyes <59646715+weweyes@users.noreply.github.com>
2021-07-03 18:16:17 +0800

    Update diy-2.sh

--- build/Lede_xiaomi_ac2100/diy-2.sh
+++ build/Lede_xiaomi_ac2100/diy-2.sh
@@ -29,7 +29,7 @@ sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-
 sed -i 's/"KMS 服务器"/"KMS 激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
-sed -i 's/TTYD 终端/命令窗/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
+sed -i 's/"TTYD 终端"/"TTYD"/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/"Web 管理"/"Web 管理"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
