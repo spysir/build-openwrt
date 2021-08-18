37314ed35066543f12100c2f5fa3181522b021bb
weweyes <59646715+weweyes@users.noreply.github.com>
2021-08-18 13:48:52 +0800

    Update diy-2.sh

--- build/Lede_xiaomi_ac2100/diy-2.sh
+++ build/Lede_xiaomi_ac2100/diy-2.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-# 修改openwrt登陆地址,把下面的192.168.8.5修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址,把下面的192.168.9.1修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把XMR修改你喜欢的就行（不能使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='XMR'' package/lean/default-settings/files/zzz-default-settings
