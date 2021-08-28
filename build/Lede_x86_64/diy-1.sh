0116455aec7c0dae196d8d1d40367c7d66221633
weweyes <59646715+weweyes@users.noreply.github.com>
2021-08-28 14:33:44 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -17,6 +17,6 @@ git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 git clone https://github.com/weweyes/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/weweyes/luci-app-tcpdump.git package/luci-app-tcpdump
