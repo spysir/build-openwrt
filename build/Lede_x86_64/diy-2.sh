6ae43c350e29f2bfc004ab441c7b07ac442be4b1
weweyes <59646715+weweyes@users.noreply.github.com>
2021-10-19 21:21:07 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/10.10.10.5/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把XPK修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='XPK'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='VMR'' package/lean/default-settings/files/zzz-default-settings
 
 # 内核版本号里显示一个自己的名字（XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
