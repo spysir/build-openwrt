ef69e5b25caa63b4ba4f33b54457a5735d46e964
weweyes <59646715+weweyes@users.noreply.github.com>
2021-10-19 21:21:31 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -5,7 +5,7 @@
 # 修改openwrt登陆地址,把下面的10.10.10.5修改成你想要的就可以了
 sed -i 's/192.168.1.1/10.10.10.5/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把XPK修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字，把VMR修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='VMR'' package/lean/default-settings/files/zzz-default-settings
 
 # 内核版本号里显示一个自己的名字（XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
