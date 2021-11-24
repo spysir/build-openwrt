7ad6ecd808ff78678e3f8c2e599d9a2321207fdf
spysir <59646715+spysir@users.noreply.github.com>
2021-11-24 12:28:34 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -7,15 +7,15 @@
 # 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
 # 软件包地址：https://github.com/281677160/openwrt-package
-#git clone https://github.com/weweyes/OpenAppFilter.git package/luci-app-oaf
+#git clone https://github.com/spysir/OpenAppFilter.git package/luci-app-oaf
 #svn co https://github.com/281677160/openwrt-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
 
-git clone https://github.com/weweyes/luci-theme-opentopd package/luci-theme-opentopd
+git clone https://github.com/spysir/luci-theme-opentopd package/luci-theme-opentopd
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
 git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-git clone https://github.com/weweyes/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/weweyes/luci-app-tcpdump.git package/luci-app-tcpdump
+git clone https://github.com/spysir/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/spysir/luci-app-tcpdump.git package/luci-app-tcpdump
